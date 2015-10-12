4356e492e350994c62db89b3426f198610ebf707
Sten Roger Sandvik <srs@enonic.com>
2015-10-12 07:28:17 +0200

    Removed cli since we do not need it.

--- src/main/java/com/enonic/cms2xp/Main.java
+++ src/main/java/com/enonic/cms2xp/Main.java
@@ -2,42 +2,17 @@ package com.enonic.cms2xp;
 
 import java.io.File;
 
-import javax.inject.Inject;
-
-import io.airlift.airline.Command;
-import io.airlift.airline.HelpOption;
-import io.airlift.airline.Option;
-import io.airlift.airline.SingleCommand;
-
 import com.enonic.cms2xp.config.MainConfig;
 import com.enonic.cms2xp.config.MainConfigLoader;
 import com.enonic.cms2xp.migrate.ExportData;
 
-@Command(name = "cms2xp", description = "Migrates data from CMS to XP")
 public final class Main
 {
-    @Inject
-    public HelpOption helpOption;
-
-    @Option(name = "-c", description = "Config file to use.", required = true)
-    public File configFile;
-
     public static void main( final String... args )
         throws Exception
     {
-        final Main main = SingleCommand.singleCommand( Main.class ).parse( args );
-        if ( main.helpOption.showHelpIfRequested() )
-        {
-            return;
-        }
-
-        main.run();
-    }
-
-    public void run()
-        throws Exception
-    {
-        final MainConfigLoader loader = new MainConfigLoader( this.configFile.toURI().toURL() );
+        final File configFile = new File( args[0] );
+        final MainConfigLoader loader = new MainConfigLoader( configFile.toURI().toURL() );
         final MainConfig config = loader.load();
         new ExportData( config ).execute();
     }
