391ce68f6055c419c8970a029db454ffa97f70fb
Alex Rodriguez <aro@enonic.com>
2016-01-07 08:49:16 +0100

    EXT-95 Improve detection of media types

--- src/main/java/com/enonic/xp/util/MediaTypes.java
+++ src/main/java/com/enonic/xp/util/MediaTypes.java
@@ -18,15 +18,58 @@ public final class MediaTypes
         put( "png", MediaType.PNG );
         put( "jpeg", MediaType.JPEG );
         put( "jpg", MediaType.JPEG );
+        put( "tiff", MediaType.TIFF );
+        put( "tif", MediaType.TIFF );
+        put( "bmp", MediaType.BMP );
         put( "pdf", MediaType.PDF );
         put( "json", MediaType.JSON_UTF_8 );
         put( "js", MediaType.JAVASCRIPT_UTF_8 );
         put( "css", MediaType.CSS_UTF_8 );
         put( "html", MediaType.HTML_UTF_8 );
+        put( "htm", MediaType.HTML_UTF_8 );
         put( "xml", MediaType.XML_UTF_8 );
         put( "svg", MediaType.SVG_UTF_8 );
         put( "mp4", MediaType.MP4_VIDEO );
         put( "swf", MediaType.SHOCKWAVE_FLASH );
+
+        // Audio
+        put( "mp3", MediaType.MPEG_AUDIO );
+
+        // Video
+        put( "avi", MediaType.MPEG_VIDEO );
+        put( "mpeg", MediaType.MPEG_VIDEO );
+
+        // Archive
+        put( "zip", MediaType.ZIP );
+        put( "gzip", MediaType.ZIP );
+        put( "gz", MediaType.ZIP );
+        put( "rar", MediaType.create( "application", "x-rar-compressed" ) );
+
+        // Text
+        put( "txt", MediaType.PLAIN_TEXT_UTF_8 );
+        put( "csv", MediaType.CSV_UTF_8 );
+
+        // Data
+        put( "rtf", MediaType.RTF_UTF_8 );
+
+        // Document
+        put( "odt", MediaType.OPENDOCUMENT_TEXT );
+        put( "docx", MediaType.MICROSOFT_WORD );
+        put( "doc", MediaType.MICROSOFT_WORD );
+
+        // Presentation
+        put( "odp", MediaType.OPENDOCUMENT_PRESENTATION );
+        put( "pptx", MediaType.MICROSOFT_POWERPOINT );
+        put( "ppsx", MediaType.MICROSOFT_POWERPOINT );
+        put( "ppt", MediaType.MICROSOFT_POWERPOINT );
+
+        // Spreadsheet
+        put( "ods", MediaType.OPENDOCUMENT_SPREADSHEET );
+        put( "xlsx", MediaType.MICROSOFT_EXCEL );
+        put( "xls", MediaType.MICROSOFT_EXCEL );
+
+        // Vector
+        put( "eps", MediaType.POSTSCRIPT );
     }
 
     public MediaType fromExt( final String ext )
