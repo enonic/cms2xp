2b23edc91704318da95d3d2e4f334ed38942727f
Alex Rodriguez <aro@enonic.com>
2016-09-19 15:29:01 +0200

    Updated to next snapshot version

--- src/main/java/com/enonic/cms2xp/converter/HtmlAreaConverter.java
+++ src/main/java/com/enonic/cms2xp/converter/HtmlAreaConverter.java
@@ -218,7 +218,7 @@ public class HtmlAreaConverter
         figureEl.appendChild( img );
 
         String caption = img.attr( "title" );
-        if ( StringUtils.isBlank( caption ) )
+        if ( StringUtils.isBlank( caption ) )//
         {
             caption = img.attr( "alt" );
         }
